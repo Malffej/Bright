a012228ae77dceb2760d0da63e1b9d3cdc26843b
Jeff Lam <jeff.lam1990@gmail.com>
2014-07-15 21:39:18 -0700

    Added logout button and completed signup/login process

--- gen/com/malffej/bright/R.java
+++ gen/com/malffej/bright/R.java
@@ -25,7 +25,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
+        public static final int action_logout=0x7f080009;
         public static final int loginActivityLoginButton=0x7f080002;
         public static final int loginActivityPasswordEditText=0x7f080001;
         public static final int loginActivitySignupTextView=0x7f080003;
@@ -42,12 +42,10 @@ public final class R {
         public static final int activity_signup=0x7f030002;
     }
     public static final class menu {
-        public static final int login=0x7f070000;
-        public static final int main=0x7f070001;
-        public static final int signup=0x7f070002;
+        public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
+        public static final int action_logout=0x7f050001;
         /**  Login Activity Strings 
          */
         public static final int app_name=0x7f050000;
--- src/com/malffej/bright/MainActivity.java
+++ src/com/malffej/bright/MainActivity.java
@@ -1,36 +1,55 @@
 package com.malffej.bright;
 
+import com.parse.ParseUser;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	protected static final String TAG = MainActivity.class.getSimpleName();
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if (currentUser == null) {
+			navigateToLogin();
+		} else {
+			Log.i(TAG, currentUser.getUsername());
+		}
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	private void navigateToLogin() {
+		Intent intent = new Intent(this, LoginActivity.class);
+		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+		startActivity(intent);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int itemId = item.getItemId();
+
+		switch (itemId) {
+		case R.id.action_logout:
+			ParseUser.logOut();
+			navigateToLogin();
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
